d0d0a7451090ca488f90eb2e96b76207564260cb
jhyungit <jh021199@gmail.com>
2026-02-16 11:28:44 +0900

    commit

---
+++ src/swea/p1248/Solution_Jeonghyun.java
@@ -0,0 +1,13 @@
+package swea.p1248; //공통조상
+//https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15PTkqAPYCFAYD&
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution_Jeonghyun{
+	public static void main(String[] args)  throws Exception{
+
+	}
+
+}
