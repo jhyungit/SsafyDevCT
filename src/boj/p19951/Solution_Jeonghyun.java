4523bdad0e635b1eec6302eca73b71d63a539fa9
jhyungit <jh021199@gmail.com>
2026-03-15 17:28:56 +0900

    commit

---
+++ src/boj/p19951/Solution_Jeonghyun.java
@@ -0,0 +1,15 @@
+package boj.p19951;
+// 태상이의 훈련소 생활
+//https://www.acmicpc.net/problem/19951
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution_Jeonghyun{
+	
+	public static void main(String[] args)  throws Exception{
+
+	}
+
+}
