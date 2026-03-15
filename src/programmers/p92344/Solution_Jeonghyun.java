fd634cc5a0ae1f280d1393e11e4639a2732d7fbc
jhyungit <92933743+jhyungit@users.noreply.github.com>
2026-03-15 17:31:59 +0900

    ADD PROGRAMMERS FOLDER

---
+++ src/programmers/p92344/Solution_Jeonghyun.java
@@ -0,0 +1,15 @@
+package programmers.p92344;
+// 파괴되지 않은 건물
+//https://school.programmers.co.kr/learn/courses/30/lessons/92344
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution_Jeonghyun{
+	
+	public static void main(String[] args)  throws Exception{
+
+	}
+
+}
