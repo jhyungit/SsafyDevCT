9c17fbec3e7a73dce8147f1979b56ebd6ecede6c
jhyungit <jh021199@gmail.com>
2026-03-15 17:24:21 +0900

    ADD boj folder

---
+++ src/boj/p15724/Solution_Jeonghyun.java
@@ -0,0 +1,45 @@
+package swea.p1288; //새로운 불면증 치료법
+//https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV18_yw6I9MCFAZN
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution_Jeonghyun{
+	public static void main(String[] args)  throws Exception{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int test_case = Integer.parseInt(br.readLine().trim());
+		
+		for(int i=1; i<=test_case; i++) {
+			int n = Integer.parseInt(br.readLine().trim()); // 입력한 숫자
+			int k = 1;
+			
+			Set<Character> set = new HashSet<>();
+			
+			while(set.size()!=10) {
+				int kn = n*k++;
+				String numString = String.valueOf(kn);
+				
+				for(int j = 0; j < numString.length(); j++) {
+					char c = numString.charAt(j);
+						set.add(c);
+				}
+			}
+			System.out.println("#"+i+" "+ ((n*k)-n));
+		}
+	}
+
+}
+
+//5
+//1
+//2
+//11
+//1295
+//1692
+
+//#1 10
+//#2 90
+//#3 110
+//#4 6475
+//#5 5076
