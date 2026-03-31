4b311acc4110eaba94304207e66e92efc96fb580
송희 <dlqnslthdgml@naver.com>
2026-03-31 22:59:43 +0900

    송희_키로거

--- src/boj/p5397/Solution_Songhee.java
+++ src/boj/p5397/Solution_Songhee.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 메모리 : 122664KB
+// 시간 : 516ms
 public class Solution_Songhee {
     static class Node {
         char word;
@@ -20,33 +22,49 @@ public class Solution_Songhee {
 
         for (int tc = 0; tc < T; tc++) {
             String str = br.readLine();
+            start = new Node(); // 빈값
+            start.word = ' ';
+            curr = start;
+
             for (int i = 0; i < str.length(); i++) {
                 char c = str.charAt(i);
 
                 if (c == '<') {
-                    if (curr.prev != null)
+                    if (curr != start) // prev로 체크말고 start 아닐 경우에만
                         curr = curr.prev;
                 } else if (c == '>') {
                     if (curr.next != null)
                         curr = curr.next;
                 } else if (c == '-') { // 직전 단어 삭제
-                    if (curr.prev != null)
+                    if (curr != start) {
                         curr.prev.next = curr.next;
-                    if (curr.next != null)
-                        curr.next.prev = curr.prev;
-                    curr = curr.prev;
+                        if (curr.next != null)
+                            curr.next.prev = curr.prev;
+                        curr = curr.prev;
+                    }
                 } else {
                     Node node = new Node();
                     node.word = c;
                     node.prev = curr;
 
                     if (curr.next != null) {
+                        node.next = curr.next;
                         curr.next.prev = node;
                     }
                     curr.next = node;
                     curr = node;
                 }
             }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (Node n = start; n != null; n = n.next) {
+                if (n.word != ' ') {
+                    sb.append(n.word);
+                }
+            }
+
+            System.out.println(sb);
         }
     }
 }
