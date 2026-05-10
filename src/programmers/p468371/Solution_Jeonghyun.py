610b4899487707384007b5785ba60527a21eb6c6
Jeonghyun Lee <92933743+jhyungit@users.noreply.github.com>
2026-05-10 15:37:56 +0900

    "260513 노란불 신호등"
    Implement LCM calculation and yellow light check.

--- src/programmers/p468371/Solution_Jeonghyun.py
+++ src/programmers/p468371/Solution_Jeonghyun.py
@@ -1,6 +1,60 @@
+from math import gcd # 최대공약수
+from functools import reduce # 누적계산
+
+# 두 수의 최소공배수 구하기
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
+def is_all_yellow(lights, time):
+    return all(light[(time - 1) % len(light)] == 'Y' for light in lights)
+
+def solution(signals):
+    lights = ['G'*g + 'Y'*y + 'R'*r for g,y,r in signals] # ['GGYRR', 'GGGGGYR'] 형태
+    periods = [len(light) for light in lights] # 각 주기
+    
+     # 모든 주기의 최소공배수
+    limit = reduce(lcm, periods)
+    
+    time = 1
+    while time <= limit:
+        if is_all_yellow(lights, time):
+            return time
+        time += 1
+    return -1
+
+# 정확성  테스트
+# 테스트 1 〉	통과 (0.01ms, 8.91MB)
+# 테스트 2 〉	통과 (0.02ms, 9.04MB)
+# 테스트 3 〉	통과 (0.03ms, 8.96MB)
+# 테스트 4 〉	통과 (0.28ms, 9.16MB)
+# 테스트 5 〉	통과 (0.02ms, 9.08MB)
+# 테스트 6 〉	통과 (0.01ms, 9.16MB)
+# 테스트 7 〉	통과 (0.01ms, 9.14MB)
+# 테스트 8 〉	통과 (0.01ms, 9.16MB)
+# 테스트 9 〉	통과 (0.01ms, 9.19MB)
+# 테스트 10 〉	통과 (0.01ms, 9.24MB)
+# 테스트 11 〉	통과 (0.27ms, 8.96MB)
+# 테스트 12 〉	통과 (0.01ms, 9.16MB)
+# 테스트 13 〉	통과 (0.03ms, 9.08MB)
+# 테스트 14 〉	통과 (1.02ms, 9.14MB)
+# 테스트 15 〉	통과 (0.83ms, 8.89MB)
+# 테스트 16 〉	통과 (11.10ms, 8.97MB)
+# 테스트 17 〉	통과 (3.90ms, 9.12MB)
+# 테스트 18 〉	통과 (78.28ms, 9.09MB)
+# 테스트 19 〉	통과 (66.89ms, 9.2MB)
+# 테스트 20 〉	통과 (145.84ms, 9.2MB)
+# 테스트 21 〉	통과 (79.66ms, 9.05MB)
+# 테스트 22 〉	통과 (147.32ms, 8.96MB)
+# 테스트 23 〉	통과 (196.28ms, 9.16MB)
+# 테스트 24 〉	통과 (260.02ms, 9.09MB)
+# 테스트 25 〉	통과 (316.18ms, 9.2MB)
+# 채점 결과
+# 정확성: 100.0
+# 합계: 100.0 / 100.0
+
+
 # 꼼수 풀이..
 # 이렇게 풀면 혼나요~
-
 def solution(signals):
     answer = 0
     light = []
